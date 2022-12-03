ddf6865659dc040d5f5076f74bda80d76f030711
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-03 01:09:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "provaes7.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int n=0;
+    cin >> n;
+    if (primo (n,2) == true) {
+        cout << "numero primo";
+    }
+    else{
+        cout << "numero non primo";
+    }
+    return 0;
 }
