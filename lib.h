dd4ec7bb7f310bb454f8acc80d11859d685b8dd5
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-03 01:25:33 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,8 @@
-#ifndef PROVA_PROVAES7_H
-#define PROVA_PROVAES7_H
-bool primo (int,int);
-#endif //PROVA_PROVAES7_H
+//
+// Created by franc on 03/12/2022.
+//
+
+#ifndef ES6_LIB_H
+#define ES6_LIB_H
+bool funzione (int,int);
+#endif //ES6_LIB_H
