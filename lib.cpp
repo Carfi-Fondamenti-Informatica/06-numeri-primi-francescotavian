e32292be85606dbf74d94050c46544f65fb202f8
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-03 01:10:41 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "provaes7.h"
 
+
+bool primo (int n, int i)
+{
+    for ( i=2; i<n; i++ )
+    {
+        if (n%i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
